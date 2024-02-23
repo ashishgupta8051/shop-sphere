e1b76a9f174bc4dffbe3f9131f2dcf5110e53631
AshishGupta <ashish.gupta@sign3labs.com>
2024-02-23 20:32:51 +0530

    add utils file and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
+
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-
-      ),
-      darkTheme: ThemeData(),
-    );
-  }
-}
 
