211b74e4682192a0ea94cd2de6dbe1a37616e19c
Ashish Gupta <59619397+ashishgupta8051@users.noreply.github.com>
2025-03-18 17:30:09 +0530

    Development (#18)
    * Add Sign3 SDK in Shop Sphere
    
    * issue fix of sin3intelligence sdk integration
    
    * minor changes
    
    ---------
    
    Co-authored-by: Ashish Gupta <ashish.gupta@sign3labs.com>

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,21 @@
 import 'package:ShopSphere/navigation_menu.dart';
+import 'package:ShopSphere/utils/logging/log.dart';
 import 'package:ShopSphere/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_intelligence_sign3/flutter_intelligence_sign3.dart';
+import 'package:flutter_intelligence_sign3/model/options.dart';
+import 'package:flutter_intelligence_sign3/model/update_options.dart';
 import 'package:get/get.dart';
 
 import 'features/authentication/screens/onboarding/onboard_screen.dart';
 
-
+/// Stateless
 class App extends StatelessWidget {
   const App({super.key});
+
+  Future<void> initializeSDK() async {
+    await initSign3Sdk();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,5 +27,221 @@ class App extends StatelessWidget {
       home: const OnBoardingScreen(),
       // home: const NavigationMenu(),
     );
+    // return FutureBuilder(
+    //   future: initializeSDK(),
+    //   builder: (context, snapshot) {
+    //     return GetMaterialApp(
+    //       debugShowCheckedModeBanner: false,
+    //       themeMode: ThemeMode.system,
+    //       theme: SAppTheme.lightTheme,
+    //       darkTheme: SAppTheme.darkTheme,
+    //       home: const OnBoardingScreen(),
+    //     );
+    //   },
+    // );
+  }
+
+  UpdateOptions getUpdatedOptions(String phone, String id) {
+    Map<String, String> additionalAttributes = {
+      "TRANSACTION_ID": "76381256165476154713",
+      "DEPOSIT": "5000",
+      "WITHDRAWAL": "2000",
+      "METHOD": "UPI",
+      "STATUS": "SUCCESS",
+      "CURRENCY": "INR",
+      "TIMESTAMP": DateTime.now().millisecondsSinceEpoch.toString(),
+    };
+
+    UpdateOptions updateOptions = UpdateOptionsBuilder()
+        .setPhoneNumber(phone)
+        .setUserId(id)
+        .setPhoneInputType(PhoneInputType.GOOGLE_HINT)
+        .setOtpInputType(OtpInputType.AUTO_FILLED)
+        .setUserEventType(UserEventType.TRANSACTION)
+        .setMerchantId("1234567890")
+        .setAdditionalAttributes(additionalAttributes)
+        .build();
+    return updateOptions;
+  }
+
+  Options getOptions() {
+    Options options = OptionsBuilder()
+        .setClientId("test_tenant_flutter")
+        .setClientSecret("secret-3c23efa9-22af-47ff-bd64-42b266d4bf55")
+        .setEnvironment(Environment.PROD)
+        .build();
+    return options;
+  }
+
+  Future<void> initSign3Sdk() async {
+    var initAsync = await Sign3Intelligence.initAsync(getOptions());
+    Log.i("TAG_INIT_ASYNC", initAsync.toString());
+    var stopResult = await Sign3Intelligence.stop();
+    Log.i("TAG_STOP", stopResult.toString());
+    var id = await Sign3Intelligence.getSessionId();
+    Log.i("TAG_INIT_SESSION_ID", id.toString());
+  }
+
+  Future<void> getIntelligence() async {
+    try {
+      var init = await Sign3Intelligence.isSdkInitialized();
+      Log.i("TAG_INIT_ASYNC_RESULT", init.toString());
+      if (init){
+        var intelligence = await Sign3Intelligence.getIntelligence();
+        Log.i("TAG_GET_INTELLIGENCE", intelligence.toJson().toString());
+      }
+    } catch (e) {
+      Log.i("TAG_GET_INTELLIGENCE_ERROR", e.toString());
+    }
+  }
+
+  Future<void> updateOptions(UpdateOptions options) async {
+    await Sign3Intelligence.updateOptions(options);
   }
 }
+
+
+/// Stateful
+// class App extends StatefulWidget {
+//   const App({super.key});
+//
+//   @override
+//   State<App> createState() => _AppState();
+// }
+//
+// class _AppState extends State<App> {
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     initSign3Sdk();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       themeMode: ThemeMode.system,
+//       theme: SAppTheme.lightTheme,
+//       darkTheme: SAppTheme.darkTheme,
+//       home: const OnBoardingScreen(),
+//       // home: const NavigationMenu(),
+//     );
+//   }
+//
+//   void showCustomDialog(BuildContext context) {
+//     TextEditingController phoneController = TextEditingController();
+//     TextEditingController idController = TextEditingController();
+//
+//     showDialog(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: const Text('Update Options'),
+//           content: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               TextField(
+//                 controller: phoneController,
+//                 decoration: const InputDecoration(
+//                   labelText: 'Set Phone Number',
+//                 ),
+//                 keyboardType: TextInputType.number, // Set to number input type
+//               ),
+//               const SizedBox(height: 10),
+//               TextField(
+//                 controller: idController,
+//                 decoration: const InputDecoration(
+//                   labelText: 'Set ID',
+//                 ),
+//                 keyboardType: TextInputType.number, // Set to number input type
+//               ),
+//             ],
+//           ),
+//           actions: [
+//             ElevatedButton(
+//               onPressed: () async {
+//                 if (phoneController.text.isEmpty || idController.text.isEmpty) {
+//                   ScaffoldMessenger.of(context).showSnackBar(
+//                     const SnackBar(
+//                       content: Text('Both fields must be filled out.'),
+//                     ),
+//                   );
+//                   return;
+//                 }
+//                 await updateOptions(getUpdatedOptions(phoneController.text, idController.text));
+//                 await getIntelligence();
+//                 Navigator.of(context).pop();
+//               },
+//               child: const Text('Update Options'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+//
+//   UpdateOptions getUpdatedOptions(String phone, String id) {
+//     Map<String, String> additionalAttributes = {
+//       "TRANSACTION_ID": "76381256165476154713",
+//       "DEPOSIT": "5000",
+//       "WITHDRAWAL": "2000",
+//       "METHOD": "UPI",
+//       "STATUS": "SUCCESS",
+//       "CURRENCY": "INR",
+//       "TIMESTAMP": DateTime.now().millisecondsSinceEpoch.toString(),
+//     };
+//
+//     UpdateOptions updateOptions = UpdateOptionsBuilder()
+//         .setPhoneNumber(phone)
+//         .setUserId(id)
+//         .setPhoneInputType(PhoneInputType.GOOGLE_HINT)
+//         .setOtpInputType(OtpInputType.AUTO_FILLED)
+//         .setUserEventType(UserEventType.TRANSACTION)
+//         .setMerchantId("1234567890")
+//         .setAdditionalAttributes(additionalAttributes)
+//         .build();
+//     return updateOptions;
+//   }
+//
+//   Options getOptions() {
+//     Options options = OptionsBuilder()
+//         .setClientId("test_tenant_flutter")
+//         .setClientSecret("secret-3c23efa9-22af-47ff-bd64-42b266d4bf55")
+//         .setEnvironment(Environment.PROD)
+//         .build();
+//     return options;
+//   }
+//
+//   Future<void> initSign3Sdk() async {
+//     Log.i("TAG_INIT_ASYNC_BEFORE", "TAG_INIT_ASYNC_BEFORE");
+//     var initAsync = await Sign3Intelligence.initAsync(getOptions());
+//     Log.i("TAG_INIT_ASYNC", initAsync.toString());
+//     // var stopResult = await Sign3Intelligence.stop();
+//     // Log.i("TAG_STOP", stopResult.toString());
+//     // var id = await Sign3Intelligence.getSessionId();
+//     // Log.i("TAG_INIT_SESSION_ID", id.toString());
+//     // setState(() {
+//     //   // initValue = initAsync.toString();
+//     //   stop = stopResult.toString();
+//     //   sessionID = id;
+//     // });
+//   }
+//
+//   Future<void> getIntelligence() async {
+//     try {
+//       var init = await Sign3Intelligence.isSdkInitialized();
+//       Log.i("TAG_INIT_ASYNC_RESULT", init.toString());
+//       if (init){
+//         var intelligence = await Sign3Intelligence.getIntelligence();
+//         Log.i("TAG_GET_INTELLIGENCE", intelligence.toJson().toString());
+//       }
+//     } catch (e) {
+//       Log.i("TAG_GET_INTELLIGENCE_ERROR", e.toString());
+//     }
+//   }
+//
+//   Future<void> updateOptions(UpdateOptions options) async {
+//     await Sign3Intelligence.updateOptions(options);
+//   }
+// }
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -9,73 +9,114 @@ import 'package:ShopSphere/utils/constants/image_strings.dart';
 import 'package:ShopSphere/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_intelligence_sign3/flutter_intelligence_sign3.dart';
+import 'package:geolocator/geolocator.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
 import '../../../../utils/helpers/helper_functions.dart';
+import '../../../../utils/logging/log.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   Future<void> getIntelligence() async {
-    try {
-      var sign3IntelligenceResponse = await Sign3Intelligence.getIntelligence();
-      print("RESPONSE: ${sign3IntelligenceResponse.toJson()}");
-    } catch (e) {
-      // Handle the error message
+    // var init = await Sign3Intelligence.isSdkInitialized();
+    // if (init){
+      try {
+        // await requestLocationPermission();
+        var sign3IntelligenceResponse = await Sign3Intelligence.getIntelligence();
+        Log.i("TAG_SESSION_ID: ",await Sign3Intelligence.getSessionId());
+
+        Log.i("TAG_INTELLIGENCE_RESPONSE: ","${sign3IntelligenceResponse.toJson()}");
+      } catch (e) {
+        Log.i("TAG_GET_INTELLIGENCE_ERROR: ", e.toString());
+      }
+    // }else {
+    //   Log.i("TAG_INIT_ASYNC_ERROR: ","false");
+    // }
+  }
+
+  Future<void> requestLocationPermission() async {
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled, request the user to enable them
+      return Future.error('Location services are disabled.');
     }
+
+    // Check the current permission status
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // Request permission if it is denied
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, handle appropriately
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are permanently denied, handle appropriately
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When permissions are granted, you can access the location
+    Position position = await Geolocator.getCurrentPosition();
+    Log.i("Current position: ", "$position");
   }
 
   @override
   Widget build(BuildContext context) {
     getIntelligence();
     return Scaffold(
-        backgroundColor: SHelperFunctions.isDarkMode(context)
-            ? SColors.black
-            : SColors.white,
+      backgroundColor: SHelperFunctions.isDarkMode(context)
+          ? SColors.black
+          : SColors.white,
       body: SingleChildScrollView(
         child: Column(
           children: [
             /// Header with customised background
             SPrimaryHeaderContainer(
                 child: Column(
-              children: [
-                /// App Bar
-                const SHomeAppBar(),
-                const SizedBox(
-                  height: SSizes.spaceBtwSections,
-                ),
+                  children: [
+                    /// App Bar
+                    const SHomeAppBar(),
+                    const SizedBox(
+                      height: SSizes.spaceBtwSections,
+                    ),
 
-                /// SearchBar
-                SSearchContainer(
-                  text: "Search in store",
-                  onTap: () {},
-                ),
-                const SizedBox(
-                  height: SSizes.spaceBtwSections,
-                ),
+                    /// SearchBar
+                    SSearchContainer(
+                      text: "Search in store",
+                      onTap: () {},
+                    ),
+                    const SizedBox(
+                      height: SSizes.spaceBtwSections,
+                    ),
 
-                /// Categories, Header
-                const Padding(
-                  padding: EdgeInsets.only(left: SSizes.defaultSpace),
-                  child: Column(
-                    children: [
-                      SSectionHeading(
-                        title: "Popular Category",
-                        showActionButton: false,
-                        textColor: Colors.white,
+                    /// Categories, Header
+                    const Padding(
+                      padding: EdgeInsets.only(left: SSizes.defaultSpace),
+                      child: Column(
+                        children: [
+                          SSectionHeading(
+                            title: "Popular Category",
+                            showActionButton: false,
+                            textColor: Colors.white,
+                          ),
+                          SizedBox(
+                            height: SSizes.spaceBtwItems,
+                          ),
+                          SHomeCategories(),
+                        ],
                       ),
-                      SizedBox(
-                        height: SSizes.spaceBtwItems,
-                      ),
-                      SHomeCategories(),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: SSizes.spaceBtwSections,
-                ),
-              ],
-            )),
+                    ),
+                    const SizedBox(
+                      height: SSizes.spaceBtwSections,
+                    ),
+                  ],
+                )),
 
             /// Body part - Slider Banner & Body Product
             Padding(
@@ -94,13 +135,13 @@ class HomeScreen extends StatelessWidget {
                       height: SSizes.spaceBtwSections,
                     ),
                     /// Heading
-                     SSectionHeading(title: "Popular Products", onPressed: (){},),
+                    SSectionHeading(title: "Popular Products", onPressed: (){},),
                     const SizedBox(
                       height: SSizes.spaceBtwItems,
                     ),
                     /// Products
                     SGridLayout(itemCount: 4, itemBuilder: (_, index) =>
-                              const SProductCardVertical())
+                    const SProductCardVertical())
                   ],
                 )),
           ],
