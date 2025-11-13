a82d4b4d5e4d61d3c5800a9938cf0cc64b4471d5
Ashish Gupta <ashish.gupta@sign3labs.com>
2025-11-13 13:46:20 +0530

    minor changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,12 +9,16 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
       
-      let flutterIntelligenceSign3Plugin = FlutterIntelligenceSign3Plugin()
-      OptionsBuilder.build(
-          clientId: "test_tenant_ios",
-          secret: "secret-0ba302d5-6001-4462-a130-c413f87b689b",
-          env: OptionsBuilder.ENV_DEV)
-      flutterIntelligenceSign3Plugin.initAsync()
+      if #available(iOS 15.0, *) {
+          let flutterIntelligenceSign3Plugin = FlutterIntelligenceSign3Plugin()
+          OptionsBuilder.build(
+//            clientId: "test_tenant_ios",
+//            secret: "secret-0ba302d5-6001-4462-a130-c413f87b689b",
+            clientId: "niyo-di-ios-prod",
+            secret: "secret-95bc3c3f-af00-47e2-ad2d-d12fc09338e4",
+            env: OptionsBuilder.ENV_DEV)
+          flutterIntelligenceSign3Plugin.initAsync()
+      } 
       
       
     GeneratedPluginRegistrant.register(with: self)
